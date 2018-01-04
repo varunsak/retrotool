e120d5dc13772c26d3e972570f5884febda93377
vkoottap <vkoottap@cisco.com>
2018-01-04 14:27:41 +0530

    Admin site text changes

--- retro/admin.py
+++ retro/admin.py
@@ -4,6 +4,8 @@ from .models import *
 
 # Register your models here.
 
+admin.site.site_header = 'Retro Tool Administration'
+admin.site.site_title = 'Retro Tool Administration'
 admin.site.register(Project)
 admin.site.register(Team)
 admin.site.register(Sprint)
