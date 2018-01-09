1600241222b38045f0bf3c0067cf213a56b6377b
Saurabh Shandilya <shandilya.saurabh92@gmail.com>
2018-01-09 16:55:32 +0530

    Datepicker and status hover changes

--- retro/static/retro/js/retro.js
+++ retro/static/retro/js/retro.js
@@ -2,9 +2,17 @@ $(document).ready(function() {
     $('#retroContentTable').DataTable({
         "order": [[ 8, "desc" ]]
     });
+
     $( "#id_eta" ).click(function(){
-         $( "#id_eta" ).datepicker('show');
+        $( "#id_eta" ).datepicker('show');
     });
+
+    $( "#id_eta" ).on('changeDate', function(ev){
+        $(this).datepicker('hide');
+    });
+
+    $('[data-toggle="tooltip"]').tooltip();
+
 });
 
 function exportToCSV() {
