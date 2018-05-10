c5e35d8673b46352b9f18266a977a01b10547b1a
vkoottap <vkoottap@cisco.com>
2018-05-10 20:20:13 +0530

    Replace excel table filter with jquery database

--- retro/static/retro/js/retro.js
+++ retro/static/retro/js/retro.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-    $('#retroContentTable').DataTable({
+    /*$('#retroContentTable').DataTable({
         "bSort": false
-    });
+    });*/
 
     $( "#id_eta" ).click(function(){
         $( "#id_eta" ).datepicker('show');
@@ -13,7 +13,12 @@ $(document).ready(function() {
 
     $('[data-toggle="tooltip"]').tooltip();
 
-    $('#retroContentTable').excelTableFilter();
+    /*$('#retroContentTable').excelTableFilter();*/
+
+    $('#retroContentTable').dataTable({
+        /* No ordering applied by DataTables during initialisation */
+        "order": []
+    });
 
 });
 
